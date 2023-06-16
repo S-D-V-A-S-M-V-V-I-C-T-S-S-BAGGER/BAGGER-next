28740351f1d0a1bcdc9ecb2398857cba4f0f65ac
J.E.Loohuis <joost@loohuis.dev>
2023-06-16 12:33:26 +0200

    Read lichtingfotos folder automatically

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,14 @@
 import LogoBovenkant from '@/components/logo/LogoBovenkant';
 import LogoOnderkant from '@/components/logo/LogoOnderkant';
 import LogoMidden from '@/components/logo/LogoMidden';
-import Image from 'next/image';
-import TwoFriends from '@/public/romance-cropped.jpg';
+import LichtingFotos from '@/components/LichtingFotos';
 
 export default function Home() {
   return (
     <main>
       <LogoBovenkant/>
       <LogoMidden>
-          <div className={'lichting-links'}>
-              <div className={'lichting-afbeelding-bubbel lichting-afbeelding-links'}>
-                  <Image className={'lichting-afbeelding'} src={TwoFriends} alt={'BAGGER logo'} />
-              </div>
-          </div>
-          <div className={'lichting-rechts'}>
-              <div className={'lichting-afbeelding-bubbel lichting-afbeelding-rechts'}>
-                  <Image className={'lichting-afbeelding'} src={TwoFriends} alt={'BAGGER logo'} />
-              </div>
-          </div>
-          <div className={'lichting-links'}>
-              <div className={'lichting-afbeelding-bubbel lichting-afbeelding-links'}>
-                  <Image className={'lichting-afbeelding'} src={TwoFriends} alt={'BAGGER logo'} />
-              </div>
-          </div>
-          <div className={'lichting-rechts'}>
-              <div className={'lichting-afbeelding-bubbel lichting-afbeelding-rechts'}>
-                  <Image className={'lichting-afbeelding'} src={TwoFriends} alt={'BAGGER logo'} />
-              </div>
-          </div>
+          <LichtingFotos/>
       </LogoMidden>
       <LogoOnderkant/>
     </main>

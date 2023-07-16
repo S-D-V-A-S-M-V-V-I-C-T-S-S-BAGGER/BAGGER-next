491de419b5bcf03c163a825ade539a38ae75ffbc
J.E.Loohuis <joost@loohuis.dev>
2023-07-16 17:56:11 +0200

    Add variable density for background bubbles

--- components/BubblesTest.tsx
+++ components/BubblesTest.tsx
@@ -4,10 +4,18 @@ import {FC, useEffect, useRef, useState} from 'react';
 type Point = {
     x: number,
     y: number,
+    radius: number,
 }
 
 const maxDiscRadius = 50;
 const minDiscRadius = 20;
+const densityMultiplier = 2.1;
+
+function densityTest(point1: Point, point2: Point) {
+    const distanceSquared = (point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2;
+    const minDistanceSquared = ((point1.radius + point2.radius) * densityMultiplier) ** 2;
+    return distanceSquared < minDistanceSquared;
+}
 
 function naivePoissonDiscs(height: number, width: number, discDiameter: number, maxNumberOfDiscs: number) {
     const points: Point[] = [];
@@ -19,14 +27,15 @@ function naivePoissonDiscs(height: number, width: number, discDiameter: number, 
     const discRadius = discDiameter / 2;
 
     generatePoint: while (points.length < maxNumberOfDiscs) {
-        const candidatePoint = {
+        const candidatePoint: Point = {
             x: Math.random() * (width - discDiameter) + discRadius,
             y: Math.random() * (height - discDiameter) + discRadius,
+            radius: Math.random() * (maxDiscRadius - minDiscRadius) + minDiscRadius,
         }
 
         for (const point of points) {
-            const distanceSquared = (point.x - candidatePoint.x) ** 2 + (point.y - candidatePoint.y) ** 2;
-            if (distanceSquared <= discDiameterSquared) {
+            const tooClose = densityTest(point, candidatePoint);
+            if (tooClose) {
                 tries += 1;
 
                 if (tries >= maxRetries) {
@@ -63,9 +72,8 @@ const BubblesTest: FC = () => {
     // TODO debounced onwindowresize
 
     const circles = discPoints.map((point, index) => {
-        const radius = Math.random() * (maxDiscRadius - minDiscRadius) + minDiscRadius;
         return (
-            <circle key={index} cx={point.x} cy={point.y} r={radius} fill="var(--aarde-bovenkant-bruin)"/>
+            <circle key={index} cx={point.x} cy={point.y} r={point.radius} fill="var(--aarde-bovenkant-bruin)"/>
         )
     })
 

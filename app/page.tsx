093158b388a104cb37d094346dbb3b93febb5a6e
J.E.Loohuis <joost@loohuis.dev>
2023-07-17 13:30:21 +0200

    Add context awareness basis to procedural bubbles

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import LogoBovenkant from '@/components/logo/LogoBovenkant';
 import LogoOnderkant from '@/components/logo/LogoOnderkant';
 import LogoMidden from '@/components/logo/LogoMidden';
-import LichtingFotos from '@/components/LichtingFotos';
+import LichtingFotosLoader from '@/components/LichtingFotosLoader';
 
 export default function Home() {
   return (
     <main>
       <LogoBovenkant/>
       <LogoMidden>
-          <LichtingFotos/>
+          <LichtingFotosLoader/>
       </LogoMidden>
       <LogoOnderkant/>
     </main>
--- components/BubblesTest.tsx
+++ components/BubblesTest.tsx
@@ -1,7 +1,8 @@
 'use client'
-import {FC, useEffect, useRef, useState} from 'react';
+import {FC, useContext, useLayoutEffect, useRef, useState} from 'react';
+import {HoleContext} from '@/components/logo/LogoMidden';
 
-type Point = {
+export type BubbleDef = {
     x: number,
     y: number,
     radius: number,
@@ -11,26 +12,43 @@ const maxDiscRadius = 50;
 const minDiscRadius = 20;
 const densityMultiplier = 2.1;
 
-function densityTest(point1: Point, point2: Point) {
+function densityTest(point1: BubbleDef, point2: BubbleDef) {
     const distanceSquared = (point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2;
     const minDistanceSquared = ((point1.radius + point2.radius) * densityMultiplier) ** 2;
     return distanceSquared < minDistanceSquared;
 }
 
-function naivePoissonDiscs(height: number, width: number, discDiameter: number, maxNumberOfDiscs: number) {
-    const points: Point[] = [];
+function holeDensityTest(hole: BubbleDef, point: BubbleDef) {
+    const distanceSquared = (hole.x - point.x) ** 2 + (hole.y - point.y) ** 2;
+    const minDistanceSquared = (hole.radius + point.radius * densityMultiplier) ** 2;
+    return distanceSquared < minDistanceSquared;
+}
+
+function naivePoissonDiscs(height: number, width: number, maxNumberOfDiscs: number, holes: BubbleDef[] = []) {
+    const points: BubbleDef[] = [];
 
     const maxRetries = 100;
     let tries = 0;
 
-    const discDiameterSquared = discDiameter ** 2;
-    const discRadius = discDiameter / 2;
-
     generatePoint: while (points.length < maxNumberOfDiscs) {
-        const candidatePoint: Point = {
-            x: Math.random() * (width - discDiameter) + discRadius,
-            y: Math.random() * (height - discDiameter) + discRadius,
-            radius: Math.random() * (maxDiscRadius - minDiscRadius) + minDiscRadius,
+        const radius                    = Math.random() * (maxDiscRadius - minDiscRadius) + minDiscRadius;
+        const candidatePoint: BubbleDef = {
+            x: Math.random() * (width - (radius * 2)) + radius,
+            y: Math.random() * (height - (radius * 2)) + radius,
+            radius,
+        }
+
+        for (const hole of holes) {
+            const tooClose = holeDensityTest(hole, candidatePoint);
+            if (tooClose) {
+                tries += 1;
+
+                if (tries >= maxRetries) {
+                    break generatePoint;
+                } else {
+                    continue generatePoint;
+                }
+            }
         }
 
         for (const point of points) {
@@ -56,17 +74,30 @@ function naivePoissonDiscs(height: number, width: number, discDiameter: number, 
 const BubblesTest: FC = () => {
     const ref = useRef<SVGSVGElement>(null);
 
-    const [discPoints, setDiscPoints] = useState<Point[]>([])
+    const {holes: holeDefinitions} = useContext(HoleContext);
 
-    useEffect(() => {
+    const [discPoints, setDiscPoints] = useState<BubbleDef[]>([])
+
+    const [debugHoles, setDebugHoles] = useState<BubbleDef[]>([])
+
+    useLayoutEffect(() => {
         const boundingClientRect = ref.current!.getBoundingClientRect();
-        console.log(boundingClientRect);
-        const points = naivePoissonDiscs(boundingClientRect.height, boundingClientRect.width, maxDiscRadius * 3, 100);
-        console.log(points);
+
+        const relativeHoles: BubbleDef[] = holeDefinitions.map((point: BubbleDef | null): (BubbleDef | null) => point ? ({
+            x: point.x - boundingClientRect.x,
+            y: point.y - boundingClientRect.y,
+            radius: point.radius,
+        }) : null).filter(point => point !== null) as unknown[] as BubbleDef[];
+
+        // console.log(boundingClientRect);
+        const points = naivePoissonDiscs(boundingClientRect.height, boundingClientRect.width, 100, relativeHoles);
+        console.log(points, holeDefinitions);
         setDiscPoints(points);
+        setDebugHoles(relativeHoles);
     }, [
         ref.current?.getBoundingClientRect().width,
         ref.current?.getBoundingClientRect().height,
+        holeDefinitions,
     ])
 
     // TODO debounced onwindowresize
@@ -77,9 +108,16 @@ const BubblesTest: FC = () => {
         )
     })
 
+    const debugHoleCircles = debugHoles.map((hole, index) => {
+        return (
+            <circle key={circles.length + index} cx={hole.x} cy={hole.y} r={hole.radius + 10} fill='red'/>
+        )
+    })
+
     return (
         <svg ref={ref} className={'bubbeltjes-vlak'}>
             {circles}
+            {/*{debugHoleCircles}*/}
         </svg>
     )
 }

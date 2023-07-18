a771445374801813d79614637df1b976fa349687
J.E.Loohuis <joost@loohuis.dev>
2023-07-18 13:29:17 +0200

    Switch BAfko sheet access to Google API

--- app/api/bafko/route.ts
+++ app/api/bafko/route.ts
@@ -1,18 +1,50 @@
 'use server';
-import {NextResponse} from 'next/server';
+import { NextResponse } from 'next/server';
+import {google, sheets_v4} from 'googleapis';
+import {GaxiosResponse} from 'gaxios';
 
-const getBafkos = async () => {
-    const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${process.env.BAFKO_SHEET_ID}/values/A3:C?key=${process.env.SHEETS_API_KEY}`);
-    const responseJson = await response.json();
-    return responseJson.values;
+function getJwt() {
+    return new google.auth.JWT(
+        process.env.SERVICE_CLIENT_EMAIL, undefined, process.env.SERVICE_CLIENT_KEY,
+        ['https://www.googleapis.com/auth/spreadsheets']
+    );
+}
+
+type SheetContent =  GaxiosResponse<sheets_v4.Schema$ValueRange> | null | undefined
+
+async function getSheet(): Promise<SheetContent> {
+    const sheets = google.sheets({version: 'v4'});
+    return new Promise((resolve, reject) => {
+        sheets.spreadsheets.values.get({
+            spreadsheetId: process.env.BAFKO_SHEET_ID,
+            range: 'A3:C',
+            auth: getJwt(),
+            key: process.env.SHEETS_API_KEY,
+        }, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+        });
+    });
+}
+
+const getBafkos = async (): Promise<GaxiosResponse<sheets_v4.Schema$ValueRange>['data']['values'] | null> => {
+    const sheetData = await getSheet();
+    return sheetData?.data.values;
 };
 
 export async function GET() {
     const data = await getBafkos();
-    const random_index = Math.floor(Math.random() * data.length);
-    const random_bafko = data[random_index];
-    return NextResponse.json({
-        "BAfko": random_bafko[0],
-        "betekenis": random_bafko[2],
-    });
+    if (data) {
+        const random_index = Math.floor(Math.random() * data.length);
+        const random_bafko = data[random_index];
+        return NextResponse.json({
+            "BAfko": random_bafko[0],
+            "betekenis": random_bafko[2],
+        });
+    } else {
+        return NextResponse.json({ error: 'Could not find the sheet'}, {status: 404});
+    }
 }

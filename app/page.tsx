95be9a8a949bb6255c9fd9cf6f468ee73f0c28f5
J.E.Loohuis <joost@loohuis.dev>
2023-07-18 17:39:02 +0200

    Make lichtingfoto constants serverless

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import LogoBovenkant from '@/components/logo/LogoBovenkant';
 import LogoOnderkant from '@/components/logo/LogoOnderkant';
 import LogoMidden from '@/components/logo/LogoMidden';
-import LichtingFotosLoader from '@/components/LichtingFotosLoader';
+import LichtingFotos from '@/components/LichtingFotos';
 
 export default function Home() {
   return (
     <main>
       <LogoBovenkant/>
       <LogoMidden>
-          <LichtingFotosLoader/>
+          <LichtingFotos/>
       </LogoMidden>
       <LogoOnderkant/>
     </main>

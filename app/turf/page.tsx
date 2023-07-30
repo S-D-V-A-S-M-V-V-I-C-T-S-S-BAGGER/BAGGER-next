b3762a8fce68d04223b03fea6598c7e4b648e693
J.E.Loohuis <joost@loohuis.dev>
2023-07-30 15:05:04 +0200

    Move tally data to the Tally component for logging

--- app/turf/page.tsx
+++ app/turf/page.tsx
@@ -1,9 +1,9 @@
-'use client';
+'use server';
 
 import '../../components/turf/turf.css';
 import React from 'react';
 import Tally from '@/components/turf/Tally';
 
-export default function Turf() {
-    return (<Tally/>);
+export default async function Turf() {
+    return (<Tally pilsPrijs={parseInt(process.env.PILS_PRIJS!)}/>);
 }

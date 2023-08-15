f40a696e6b4498fbf5b30d0b5a5c9dc6b8976d00
J.E.Loohuis <joost@loohuis.dev>
2023-08-15 21:15:19 +0200

    Make turf range an environment variable

--- components/turf/tallySheet.ts
+++ components/turf/tallySheet.ts
@@ -3,10 +3,10 @@ import {appendSheetValues, getSheetValues, SheetValues} from '@/lib/GoogleApi';
 
 export async function getTallySheet(): Promise<SheetValues | null> {
         "use server";
-        return getSheetValues(process.env.TURF_SHEET_ID!, 'A1:D');
+        return getSheetValues(process.env.TURF_SHEET_ID!, process.env.TURF_SHEET_RANGE!);
 }
 
 export async function addTallyRow(row: string[][]) {
         "use server";
-        return appendSheetValues(process.env.TURF_SHEET_ID!, 'A1:D', row);
+        return appendSheetValues(process.env.TURF_SHEET_ID!, process.env.TURF_SHEET_RANGE!, row);
 }

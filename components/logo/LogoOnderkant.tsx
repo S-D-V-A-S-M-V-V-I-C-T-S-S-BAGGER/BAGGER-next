4149bc4d93c409ad8427f08024748b3c87e424f6
J.E.Loohuis <joost@loohuis.dev>
2024-02-10 23:17:23 +0100

    Add sitemap

--- components/logo/LogoOnderkant.tsx
+++ components/logo/LogoOnderkant.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Grolschje from '@/public/grolschje-optimized.svg';
 import './logo.css';
 import {HoleContext} from '@/components/logo/Logo';
+import {useRouter} from "next/navigation";
 
 const LogoOnderkant: FC = () => {
     const {setHoleRef} = useContext(HoleContext);
@@ -13,16 +14,32 @@ const LogoOnderkant: FC = () => {
         setHoleRef(0, bierGatRef);
     }, []);
 
+    const router = useRouter();
+    const routingDelay = 500; // milliseconds
+
+    const onClick = async () => {
+        // Prefetch the page
+        router.prefetch('/sitemap');
+        // Only actually navigate after the user has had some time to experience this wonderful button
+        await new Promise(resolve => {
+            setTimeout(resolve, routingDelay);
+        }).then(() => {
+            router.push('/sitemap');
+        });
+    };
+
     return (
         <div className="logo-onderkant">
             <div className="bier-tunnel-onder"/>
             <div ref={bierGatRef} className="bier-gat">
-                <Image
-                    className="biertje-afbeelding"
-                    src={Grolschje}
-                    alt="A Grolsch beer bottle"
-                    priority
-                />
+                <button className={'bierButton'} onClick={onClick}>
+                    <Image
+                        className="biertje-afbeelding"
+                        src={Grolschje}
+                        alt="A Grolsch beer bottle"
+                        priority
+                    />
+                </button>
             </div>
         </div>
     );

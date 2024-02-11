ae7dbe32877e7a4c75d72e7216ac915fa137cebb
J.E.Loohuis <joost@loohuis.dev>
2024-02-11 15:07:25 +0100

    Move wacky button styling

--- components/WackyButton.tsx
+++
@@ -1,8 +0,0 @@
-import {FC, PropsWithChildren} from 'react';
-import './wackyButton.css';
-
-const WackyButton: FC<PropsWithChildren> = ({children}) => {
-    return <button className={'wackyButton'}>{children}</button>;
-};
-
-export default WackyButton;

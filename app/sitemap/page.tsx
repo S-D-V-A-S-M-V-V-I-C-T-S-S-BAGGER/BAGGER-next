87d3031865956fd22e5a08e346a21a0eb46b28b1
J.E.Loohuis <joost@loohuis.dev>
2024-02-11 12:37:48 +0100

    Make sitemap pretty

--- app/sitemap/page.tsx
+++ app/sitemap/page.tsx
@@ -1,18 +1,23 @@
 import {FC} from "react";
+import './sitemap.css';
 
 const Index: FC = () => {
     return (
         <div>
-            <p>Links</p>
-            <a href={'/'}>Home/Smoelenboek</a><br/>
-            <a href={'/turf'}>Turflijst</a><br/>
-            <a href={'/declaratie'}>Declaratie indienen</a><br/>
-            <a href={'/planning'}>Activiteiten planning</a><br/>
-            <a href={'/sitemap'}>Hier</a><br/>
-            <p>API</p>
-            <p><a href={'/api/week'}>week</a> Geeft het huidige weeknummer.</p>
-            <p><a href={'/api/bak'}>bak</a> Bak of geen bak?</p>
-            <p><a href={'/api/bafko'}>bafko</a> Voor elke situatie wel één!</p>
+            <div className={'section'}>
+                <p className={'title'}>Links</p>
+                <a href={'/'}>Home/Smoelenboek</a><br/>
+                <a href={'/turf'}>Turflijst</a><br/>
+                <a href={'/declaratie'}>Declaratie indienen</a><br/>
+                <a href={'/planning'}>Activiteiten planning</a><br/>
+                <a href={'/sitemap'}>Hier</a><br/>
+            </div>
+            <div className={'section'}>
+                <p className={'title'}>API</p>
+                <p><a href={'/api/week'}>week</a> Geeft het huidige weeknummer.</p>
+                <p><a href={'/api/bak'}>bak</a> Bak of geen bak?</p>
+                <p><a href={'/api/bafko'}>bafko</a> Voor elke situatie wel één!</p>
+            </div>
         </div>
     );
 };

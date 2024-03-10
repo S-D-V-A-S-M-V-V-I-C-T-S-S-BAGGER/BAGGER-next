866a43781dd51b6ecb8ae2d00c0f74e3c55de851
J.E.Loohuis <joost@loohuis.dev>
2024-03-10 16:09:37 +0100

    Add attendance websocket communication

--- app/presentie/page.tsx
+++ app/presentie/page.tsx
@@ -1,68 +1,40 @@
 "use client";
-import {FC, useEffect, useRef} from "react";
+import {FC, useEffect, useRef, useState} from "react";
 import './presentie.css';
-import {useLocalStorage} from "@/lib/useLocalStorage";
-import dayjs from "dayjs";
 import {AttendanceButton} from "@/components/presentie/AttendanceButton";
-import {getActieveLeden} from "@/components/presentie/attendanceSheet";
 import {io} from "socket.io-client";
 
 type AttendanceState = {
     [name: string] : boolean,
 }
 
-const initialAttendanceCalculator = async (): Promise<AttendanceState> => {
-    const actieveLedenResponse = await getActieveLeden() as string[] | null;
-
-    const state: AttendanceState = {};
-
-    if (!actieveLedenResponse) {
-        return state;
-    }
-
-    for (const name of actieveLedenResponse) {
-        state[name] = false;
-    }
-
-    return state;
-};
-
-const togglePerson = (state: Readonly<AttendanceState>, name:string): AttendanceState => {
-    const newState = {...state};
-    newState[name] = !state[name];
-    return newState;
-};
-
-const addNewPerson = (state: Readonly<AttendanceState>, name:string): AttendanceState => {
-    // TODO add 26 person limit
-    const newState = {...state};
-    newState[name] = true;
-    return newState;
-};
+// TODO define events
+const socket = io({
+    path: '/api/websocket/presentie',
+    addTrailingSlash: false,
+});
 
 const Attendance: FC = () => {
-    const [attendance, setAttendance] = useLocalStorage<AttendanceState>('attendance', {});
+    const [attendance, setAttendance] = useState<AttendanceState>({});
     const newNameRef = useRef<HTMLInputElement>(null);
-
-    useEffect(() => {
-        initialAttendanceCalculator().then(res => setAttendance(res));
-        // getCurrentAttendance().then(res => console.log('Presentie:', res));
-    }, []);
 
     const buttons = [];
 
     for (const attendanceName in attendance) {
-        const onClick = () => setAttendance(togglePerson(attendance, attendanceName));
+        const onClick = () => {
+            socket.emit('togglePerson', attendanceName);
+        };
         buttons.push(<AttendanceButton key={attendanceName} name={attendanceName} present={attendance[attendanceName]} onClick={onClick}/>);
     }
 
     useEffect(() => {
-        const socket = io({
-            path: '/api/websocket/presentie',
-            addTrailingSlash: false,
-        });
-        socket.on('connect', () => {
-            console.log('Connected to ws');
+        socket.on('connect', () => console.log('WS Connected'));
+        socket.on('disconnect', () => console.log('WS disconnected'));
+        socket.on('pong', () => console.log('pong'));
+        socket.on('error', (err) => console.error(err));
+        socket.on('attendanceUpdate', (newAttendance) => {
+            console.log('Attendance update:', newAttendance);
+            setAttendance(newAttendance);
         });
 
         return () => {
@@ -74,18 +46,15 @@ const Attendance: FC = () => {
         <div className={'section'}>
             {buttons}
             <div>
-                <input ref={newNameRef} placeholder={'Anders...'}/><button onClick={
-                () => {
-                    setAttendance(addNewPerson(attendance, newNameRef.current!.value));
-                    newNameRef.current!.value = "";
-                }
-            }>+</button>
+                <input ref={newNameRef} placeholder={'Anders...'}/><button onClick={() => {
+                socket.emit(
+                    'addPerson',
+                    newNameRef.current!.value
+                );
+                newNameRef.current!.value = '';
+            }}>+</button>
             </div>
-            <button onClick={async () => {
-                // TODO save to sheet
-                console.log('Save:', dayjs().format('DD-MM-YYYY'), attendance);
-                setAttendance(await initialAttendanceCalculator());
-            }}>Opslaan</button>
+            <button onClick={() => socket.emit('newAttendance')}>Nieuwe Lijst</button>
         </div>
     );
 };
--- pages/api/websocket/presentie.ts
+++ pages/api/websocket/presentie.ts
@@ -1,6 +1,12 @@
 "use server";
 import {NextApiRequest, NextApiResponse} from "next";
 import {Server} from "socket.io";
+import {
+    addPerson,
+    createCurrentAttendance,
+    getCurrentAttendance,
+    togglePerson,
+} from "@/components/presentie/attendanceSheet";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // @ts-ignore
@@ -12,11 +18,59 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             addTrailingSlash: false,
         });
 
-        io.on('connection', (socket) => {
+        io.on('connection', async (socket) => {
             console.log('Client connected');
             socket.on('disconnect', () => {
                 console.log('Client disconnected');
             });
+            socket.on('ping', () => {
+                console.log('ping');
+                socket.emit('pong');
+            });
+            socket.on('togglePerson', async (person: string) => {
+                console.log('Toggle', person);
+                await togglePerson(person)
+                    .then((newAttendance) => {
+                        console.log('Toggled', person);
+                        io.emit('attendanceUpdate', newAttendance);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        socket.emit('error', err.toString());
+                    });
+            });
+            socket.on('addPerson', async (person: string) => {
+                console.log('Add', person);
+                await addPerson(person)
+                    .then((newAttendance) => {
+                        console.log('Added', person);
+                        io.emit('attendanceUpdate', newAttendance);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        socket.emit('error', err.toString());
+                    });
+            });
+            socket.on('newAttendance', async () => {
+                await createCurrentAttendance()
+                    .then((newAttendance) => {
+                        console.log('Sending new attendance:', newAttendance);
+                        io.emit('attendanceUpdate', newAttendance);
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        socket.emit('error', err.toString());
+                    });
+            });
+            await getCurrentAttendance()
+                .then((newAttendance) => {
+                    console.log('Sending initial attendance:', newAttendance);
+                    socket.emit('attendanceUpdate', newAttendance);
+                })
+                .catch(err => {
+                    console.error(err);
+                    socket.emit('error', err.toString());
+                });
         });
 
         // @ts-ignore

4c8378545bd9408d3481ea4d660d7def940ed157
J.E.Loohuis <joost@loohuis.dev>
2024-05-28 16:19:06 +0200

    Add auth testpage

--- components/auth/AuthTest.tsx
+++ components/auth/AuthTest.tsx
@@ -2,21 +2,31 @@
 
 import {FC} from "react";
 import {getQuotes} from "@/lib/AuthApiTest";
+import {getSessionName} from "@/lib/session";
 
 const AuthTest: FC = async () => {
+    const name = await getSessionName() ?? "gast";
+
+    let content;
+
     try {
         const quotes = await getQuotes();
 
         if (!quotes) {
-            return <div>Unauthorised</div>;
+            content = <div>Unauthorised</div>;
+        } else {
+            content = <div>{JSON.stringify(quotes)}</div>;
         }
-
-        return <div>{JSON.stringify(quotes)}</div>;
     } catch (err) {
         console.error(err);
-        return <div>Error</div>;
+        content = <div>Error</div>;
     }
-
+    return (
+        <div>
+            Hallo {name}!
+            {content}
+        </div>
+    );
 };
 
 export default AuthTest;

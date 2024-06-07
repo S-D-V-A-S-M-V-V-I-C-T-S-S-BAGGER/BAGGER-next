71349c65745f9ef2de358cdd609bd38eb9dab5d3
J.E.Loohuis <joost@loohuis.dev>
2024-06-07 17:17:41 +0200

    Add sessions and Google authorization

---
+++ app/api/auth/callback/google/route.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import {exchangeForTokens} from "@/lib/GoogleAuth";
+import {NextRequest} from "next/server";
+import {cookies} from "next/headers";
+
+const redirect_cookie_name = 'google_redirect';
+
+export async function GET(request: NextRequest) {
+    const response = await exchangeForTokens(request);
+
+    if (response) {
+        return response;
+    }
+
+    // Navigate the user back to the page from where they logged in
+    const redirectCookie = cookies().get(redirect_cookie_name);
+    return Response.redirect(new URL(redirectCookie?.value ?? "/", request.url));
+}

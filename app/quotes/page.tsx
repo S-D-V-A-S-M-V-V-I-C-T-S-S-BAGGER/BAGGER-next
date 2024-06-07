1adef2b4b4b2cb5f3ce82ba321e9514f4edc318c
J.E.Loohuis <joost@loohuis.dev>
2024-06-07 15:35:59 +0200

    Add logout button to quote list

--- app/quotes/page.tsx
+++ app/quotes/page.tsx
@@ -1,15 +1,22 @@
 "use server";
 import {FC} from "react";
 import "./quotes.css";
-import {hasSessionUser} from "@/lib/session";
 import LoginButton from "@/components/auth/LoginButton";
 import QuoteList from "@/components/quotes/QuoteList";
+import LogoutButton from "@/components/auth/LogoutButton";
+import {hasSessionUser} from "@/lib/session";
 
 const QuotePage: FC = async () => {
     const isAuthenticated = await hasSessionUser();
 
     if (isAuthenticated) {
-        return <QuoteList/>;
+        return (
+            <div>
+                <div className="auth">
+                    <LogoutButton/>
+                </div>
+                <QuoteList/>
+            </div>);
     } else {
         return <div className="loginPage"><LoginButton/></div>;
     }

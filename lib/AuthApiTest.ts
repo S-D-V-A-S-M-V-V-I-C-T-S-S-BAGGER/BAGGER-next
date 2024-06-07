876af8b063999e321bcac5357319124ad0d7e0e1
J.E.Loohuis <joost@loohuis.dev>
2024-06-07 15:13:48 +0200

    Remove auth test page

--- lib/AuthApiTest.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import {getSheetValues} from "@/lib/PersonalGoogleApi";
-
-export async function getQuotes() {
-    'use server';
-    return getSheetValues(process.env.TEST_SHEET_ID!, "A1:A");
-}
